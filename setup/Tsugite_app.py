0b8a2763ce2b93330b3d08fc4435c5723bdfea5b
Will Pan <vpan@foxmail.com>
2021-07-27 17:33:39 +0900

    Update Tsugite_app.py

--- setup/Tsugite_app.py
+++ setup/Tsugite_app.py
@@ -139,6 +139,34 @@ class GLWidget(QGLWidget):
         elif e.button() == Qt.RightButton:
             self.show.view.end_rotation()
 
+class MovieSplashScreen(QSplashScreen):
+
+    def __init__(self, movie, parent = None):
+    
+        movie.jumpToFrame(0)
+        pixmap = QPixmap(movie.frameRect().size())
+        
+        QSplashScreen.__init__(self, pixmap)
+        self.movie = movie
+        self.movie.frameChanged.connect(self.repaint)
+    
+    def showEvent(self, event):
+        self.movie.start()
+    
+    def hideEvent(self, event):
+        self.movie.stop()
+    
+    def paintEvent(self, event):
+    
+        painter = QPainter(self)
+        pixmap = self.movie.currentPixmap()
+        self.setMask(pixmap.mask())
+        painter.drawPixmap(0, 0, pixmap)
+    
+    def sizeHint(self):
+    
+        return self.movie.scaledSize()
+
 class mainWindow(QMainWindow):
 
     def __init__(self, *args):
@@ -216,8 +244,18 @@ class mainWindow(QMainWindow):
 QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True) #enable highdpi scaling
 QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True) #use highdpi icons
 app = QApplication(sys.argv)
+movie = QMovie("tsugite_loading_3d.gif")
+splash = MovieSplashScreen(movie)
+splash.show()
+
+start = time.time()
+
+while movie.state() == QMovie.Running and time.time() < start + 1:
+    app.processEvents()
 #screen = app.screens()[0]
 #dpi = screen.physicalDotsPerInch()
 window = mainWindow()
 window.show()
+splash.finish(window)
 sys.exit(app.exec_())
+
